ab49acaff0c7b612821f76ff968bb4a73aeb6af3
Mati <mfusiek200@gmail.com>
2025-08-07 00:00:01 +0200

    Added working JS and changed a little CSS

---
+++ script.js
@@ -0,0 +1,11 @@
+function Reload() {
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then((data) => {
+        document.getElementById("id").innerHTML = `ADVICE #${data.slip.id}`;
+        document.getElementById("quote").innerHTML = `"${data.slip.advice}"`;
+    })
+    .catch(() => document.getElementById("quote").innerHTML = "Failed to load quote");
+}
+
+Reload();
